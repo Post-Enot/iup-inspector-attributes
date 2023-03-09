9fa7f1f9fc7c7489696b68e4dfbce07144e42918
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2023-03-09 03:24:30 +0300

    Добавлен атрибут ReadOnly
    Атрибут позволяет отображать, но при этом не допускать изменение значения поля в окне редактора.

---
+++ IUP Inspector Attributes/Editor/Inspector Read Only Property Drawer/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,15 @@
+﻿using IUP.Toolkits.InspectorAttributes;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public sealed class ReadOnlyPropertyDrawer : PropertyDrawer
+{
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        VisualElement propertyField = new PropertyField(property);
+        propertyField.SetEnabled(false);
+        return propertyField;
+    }
+}
---
+++ IUP Inspector Attributes/Runtime/Read Only/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+namespace IUP.Toolkits.InspectorAttributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
